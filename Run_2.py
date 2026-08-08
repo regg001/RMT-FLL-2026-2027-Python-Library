82b12c8dca3c08f7e98610e69e353e1fa1332bf3
Oliver5222 <oliver.c.ng@gmail.com>
2026-08-08 16:41:12 -0400

    Updated tank to point

--- Run_2.py
+++ Run_2.py
@@ -1,5 +1,4 @@
-from pybricks.parameters import Port
-from Robot_Class import Robot
+from Robot_Class import Robot, Port
 
 
 def main():
@@ -7,19 +6,19 @@ def main():
 
     bot.gyro_reset()
     bot.straight(475, 400, 0)
-    bot.turn_tank(45)
+    bot.turn_point(45)
     bot.move_attachment(Port.B, 90, 400)
     bot.sleepms(1000)
     bot.straight(150, 400, 45)
     bot.move_attachment(Port.B, -100, 600)
     bot.straight(-90, 400, 45)
-    bot.turn_tank(-45)
+    bot.turn_point(-45)
     bot.move_attachment(Port.B, 100, 90, False)
     bot.straight(130, 100, -45)
     bot.move_attachment(Port.B, -100, 300)
     bot.straight(35, 100, -45)
     bot.straight(-45, 100, -45)
-    bot.turn_tank(0)
+    bot.turn_point(0)
     bot.move_attachment(Port.F, -175, 400)
     bot.straight(-225, 300, 0)
     bot.straight(150, 300, 0)
